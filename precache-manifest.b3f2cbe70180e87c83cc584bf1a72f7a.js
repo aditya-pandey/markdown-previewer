1878a5c7d00b1957f8b43443eeb897a885e95a68
Aditya Pandey <adityadpandey@gmail.com>
2020-06-27 12:17:59 +0530

    Updates

---
+++ precache-manifest.b3f2cbe70180e87c83cc584bf1a72f7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af85ceffb2ea672336b7f3f398993298",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "1623c7c50a7520ff7a65",
+    "url": "/markdown-previewer/static/css/main.4ab9f0bf.chunk.css"
+  },
+  {
+    "revision": "939d89455f3fe9291249",
+    "url": "/markdown-previewer/static/js/2.1c92c4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.1c92c4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1623c7c50a7520ff7a65",
+    "url": "/markdown-previewer/static/js/main.db684edb.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
